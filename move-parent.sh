13f205e67c6c43d7668a5fd6cfaaa3241c88c619
Nguyen Marc <nguyen_marc@live.fr>
2023-11-09 22:50:10 +0100

    chore: move parent without confirmation

--- move-parent.sh
+++ move-parent.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-find ./target/*/* -type f -exec sh -c 'f="$1"; echo "mv -i "$f" "$(dirname "$f")/.."";' shell {} \; | less
+set -ex
 
-echo "Confirm move (y/n)? "
-read -r answer
-if [ "$answer" != "${answer#[Yy]}" ]; then
-  find ./target/*/* -type f -exec sh -c 'f="$1"; mv -i "$f" "$(dirname "$f")/..";' shell {} \;
-else
-  echo "Move canceled."
-fi
+find ./target/*/* -type f -exec sh -c 'f="$1"; mv -i "$f" "$(dirname "$f")/..";' shell {} \;
