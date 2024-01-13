6491a9df23df9f144b5d264763e9198bffc1dcf9
Nguyen Marc <nguyen_marc@live.fr>
2024-01-13 20:11:46 +0100

    fix(concat): skip input file instead of crashing

--- video/concat/concat.c
+++ video/concat/concat.c
@@ -59,16 +59,17 @@ int concat(const char *output_file, size_t input_files_count,
     int stream_index = 0;
 
     if ((ret = avformat_open_input(&ifmt_ctx, input_file, 0, 0)) < 0) {
-      fprintf(stderr, "Could not open input file '%s': %s\n", input_file,
-              av_err2str(ret));
-      goto end;
+      fprintf(stderr, "Could not open input file '%s': %s, skipping...\n",
+              input_file, av_err2str(ret));
+      continue;
     }
 
     // Retrieve input stream information
     if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
-      fprintf(stderr, "Failed to retrieve input stream information: %s\n",
+      fprintf(stderr,
+              "Failed to retrieve input stream information: %s, skipping...\n",
               av_err2str(ret));
-      goto end;
+      continue;
     }
 
     av_dump_format(ifmt_ctx, 0, input_file, 0);
