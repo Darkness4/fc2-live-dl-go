f6ad55070520a6f8bc6fe920e71a11549776cdbd
Nguyen Marc <nguyen_marc@live.fr>
2024-06-16 17:47:41 +0200

    feat(otel): add basic setup

--- go.mod
+++ go.mod
@@ -9,25 +9,43 @@ require (
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.9.0
 	github.com/urfave/cli/v2 v2.27.2
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.27.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.27.0
+	go.opentelemetry.io/otel/sdk v1.27.0
+	go.opentelemetry.io/otel/sdk/metric v1.27.0
 	golang.org/x/sync v0.7.0
 	gopkg.in/yaml.v3 v3.0.1
 	nhooyr.io/websocket v1.8.11
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.17.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo/v2 v2.13.0 // indirect
 	github.com/onsi/gomega v1.28.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/tools v0.14.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240520151616-dc85e6b867a5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240515191416-fc5f0ca64291 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 )
---
+++ telemetry/telemetry.go
@@ -0,0 +1,139 @@
+package telemetry
+
+import (
+	"context"
+	"errors"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+type Option func(*options)
+
+type options struct {
+	stdout         bool
+	traceExporter  trace.SpanExporter
+	metricExporter metric.Exporter
+}
+
+func WithStdout() Option {
+	return func(o *options) {
+		o.stdout = true
+	}
+}
+
+func WithTraceExporter(exporter trace.SpanExporter) Option {
+	return func(o *options) {
+		o.traceExporter = exporter
+	}
+}
+
+func WithMetricExporter(exporter metric.Exporter) Option {
+	return func(o *options) {
+		o.metricExporter = exporter
+	}
+}
+
+func applyOptions(opts []Option) *options {
+	opt := &options{}
+	for _, o := range opts {
+		o(opt)
+	}
+	return opt
+}
+
+func SetupOTELSDK(
+	ctx context.Context,
+	opts ...Option,
+) (
+	shutdown func(context.Context) error,
+	err error,
+) {
+	o := applyOptions(opts)
+	var shutdownFuncs []func(context.Context) error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up propagator.
+	propagator := newPropagator()
+	otel.SetTextMapPropagator(propagator)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider(o)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	meterProvider, err := newMeterProvider(o)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	return
+}
+
+//nolint:ireturn
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider(o *options) (*trace.TracerProvider, error) {
+	var opts []trace.TracerProviderOption
+	if o.stdout {
+		traceExporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+		if err != nil {
+			return nil, err
+		}
+		opts = append(opts, trace.WithBatcher(traceExporter))
+	}
+	if o.traceExporter != nil {
+		opts = append(opts, trace.WithBatcher(o.traceExporter))
+	}
+	traceProvider := trace.NewTracerProvider(opts...)
+	return traceProvider, nil
+}
+
+func newMeterProvider(o *options) (*metric.MeterProvider, error) {
+	var opts []metric.Option
+	if o.stdout {
+		metricExporter, err := stdoutmetric.New()
+		if err != nil {
+			return nil, err
+		}
+		opts = append(opts, metric.WithReader(metric.NewPeriodicReader(metricExporter)))
+	}
+	if o.metricExporter != nil {
+		opts = append(opts, metric.WithReader(metric.NewPeriodicReader(o.metricExporter)))
+	}
+	meterProvider := metric.NewMeterProvider(opts...)
+	return meterProvider, nil
+}
