2f23bb06e36853e0a016f0a4f510687c6b08e6f5
Nguyen Marc <nguyen_marc@live.fr>
2024-07-20 14:49:50 +0200

    Revert "chore(trace): remove unimportant trace"
    This reverts commit 02eb8caa823cede1ec3bba8646510b51db827a21.

--- fc2/fc2_websocket.go
+++ fc2/fc2_websocket.go
@@ -86,6 +86,10 @@ func (w *WebSocket) GetHLSInformation(
 	conn *websocket.Conn,
 	msgChan <-chan *WSResponse,
 ) (*HLSInformation, error) {
+	ctx, span := otel.Tracer(tracerName).Start(ctx, "ws.GetHLSInformation", trace.WithAttributes(
+		attribute.String("url", w.url),
+	))
+	defer span.End()
 	msgObj, err := w.sendMessageAndWaitResponse(
 		ctx,
 		conn,
@@ -95,11 +99,15 @@ func (w *WebSocket) GetHLSInformation(
 		5*time.Second,
 	)
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
 
 	var arguments HLSInformation
 	if err := json.Unmarshal(msgObj.Arguments, &arguments); err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
 		return nil, err
 	}
 	if len(arguments.Playlists) > 0 {
