febaf26ec4f69cc61760c36463358e7c8bd0f275
Nguyen Marc <nguyen_marc@live.fr>
2024-09-28 21:50:10 +0200

    fix(utils): return EOF first before Canceled

--- utils/errors.go
+++ utils/errors.go
@@ -9,15 +9,22 @@ import (
 // GetFirstValuableErrorOrFirst returns the first error that is not nil, not EOF, and not context.Canceled. If not found, returns the first error.
 func GetFirstValuableErrorOrFirst(errs []error) error {
 	var nonNilErr error
+	var firstEOF error
 	for _, err := range errs {
 		if err != nil {
 			if nonNilErr == nil {
 				nonNilErr = err
 			}
+			if firstEOF == nil && errors.Is(err, io.EOF) {
+				firstEOF = err
+			}
 			if !errors.Is(err, io.EOF) && !errors.Is(err, context.Canceled) {
 				return err
 			}
 		}
+	}
+	if firstEOF != nil {
+		return firstEOF
 	}
 	return nonNilErr
 }
