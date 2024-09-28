052443482ed98f1b8b3905c189edf01e4e621563
Nguyen Marc <nguyen_marc@live.fr>
2024-09-28 21:51:32 +0200

    chore(utils): set a better name name firsteof

--- utils/errors.go
+++ utils/errors.go
@@ -9,22 +9,22 @@ import (
 // GetFirstValuableErrorOrFirst returns the first error that is not nil, not EOF, and not context.Canceled. If not found, returns the first error.
 func GetFirstValuableErrorOrFirst(errs []error) error {
 	var nonNilErr error
-	var firstEOF error
+	var eof error
 	for _, err := range errs {
 		if err != nil {
 			if nonNilErr == nil {
 				nonNilErr = err
 			}
-			if firstEOF == nil && errors.Is(err, io.EOF) {
-				firstEOF = err
+			if eof == nil && errors.Is(err, io.EOF) {
+				eof = err
 			}
 			if !errors.Is(err, io.EOF) && !errors.Is(err, context.Canceled) {
 				return err
 			}
 		}
 	}
-	if firstEOF != nil {
-		return firstEOF
+	if eof != nil {
+		return eof
 	}
 	return nonNilErr
 }
